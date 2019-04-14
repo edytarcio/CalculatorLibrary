5b7da4965e747e1e6f0bb7b2b7c4926206abc908
Edytarcio <edytarcio@gmail.com>
2019-04-14 20:55:11 -0300

    Add multiplication function for library

--- calculator.py
+++ calculator.py
@@ -9,3 +9,7 @@ def add(first_operator, second_operator):
 
 def subtract(first_operator, second_operator):
     return first_operator - second_operator
+
+
+def multiplication(first_operator, second_operator):
+    return first_operator * second_operator
