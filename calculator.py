bf33c95939b88dabcfb2d7fb4fa3b1dc889bf4cd
Edytarcio <edytarcio@gmail.com>
2019-04-14 18:52:11 -0300

    Add basic operations for addtion and subtraction

---
+++ calculator.py
@@ -0,0 +1,9 @@
+"""
+Calculator library containing basic math operations.
+"""
+
+def add(first_operator, second_operator):
+    return first_operator + second_operator
+
+def subtract(first_operador, second_operator):
+    return first_operator - second_operator
